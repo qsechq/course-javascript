fecba6ea99af28f20c9b03b488d85d66a0a03a58
Nikita <ss16nik@gmail.com>
2022-03-31 15:13:16 +0400

    fix dz

--- projects/dnd/functions.js
+++ projects/dnd/functions.js
@@ -78,7 +78,15 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-  target.addEventListener('click', fn, { once: true });
+  let onceRun = true;
+  target.addEventListener('click', () => {
+    if (onceRun === true) {
+      fn();
+      onceRun = false;
+    } else if (onceRun === false) {
+      target.removeEventListener('click', fn);
+    }
+  });
 }
 
 export { addListener, removeListener, skipDefault, emulateClick, delegate, once };
